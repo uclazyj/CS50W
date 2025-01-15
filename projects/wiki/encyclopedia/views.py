88afef49be3cc11320de94edd1f94e6f9a4b056f
yujianzhao <zhaoyujian@ucla.edu>
2025-01-15 15:24:49 -0500

    Finish "Entry Page" part of the project

--- projects/wiki/encyclopedia/views.py
+++ projects/wiki/encyclopedia/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown import markdown
+
 
 def index(request):
+    print("foooo!")
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        return render(request, "encyclopedia/error.html")
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "body": markdown(entry)
+    })
