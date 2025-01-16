f5494ca97ed6b4d770eb1151d440dda56238eea2
yujianzhao <zhaoyujian@ucla.edu>
2025-01-16 12:08:31 -0500

    Finish Search part of project 1.

--- projects/wiki/encyclopedia/views.py
+++ projects/wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from . import util
 
@@ -6,6 +8,18 @@ from markdown import markdown
 
 
 def index(request):
+    query = request.GET.get("q","")
+    if query != "":
+        all_entries = util.list_entries()
+        all_entries_lowercase = [entry.lower() for entry in all_entries]
+        # Exact match (case-insensitive)
+        if query.lower() in all_entries_lowercase:
+            return redirect("title", title=query)
+        # Match to a substring of an entry / entries
+        matched_entries = [entry for entry in all_entries if query.lower() in entry.lower()]
+        return render(request, "encyclopedia/search_results_page.html", {
+            "matched_entries": matched_entries
+        })
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
