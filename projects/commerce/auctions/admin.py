6f2a491a938bbb8d1d568558a9d638f3a5802595
yujianzhao <zhaoyujian@ucla.edu>
2025-01-29 21:24:07 -0500

    Add admin control of Bid, Watch, and Comment objects.

--- projects/commerce/auctions/admin.py
+++ projects/commerce/auctions/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import User, Listing
+from .models import User, Listing, Bid, Watch, Comment
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Watch)
+admin.site.register(Comment)
