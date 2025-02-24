b54a851b6560acd0f1c74def56882eb3f75c6ccc
yujianzhao <zhaoyujian@ucla.edu>
2025-02-24 12:02:53 -0500

    Refactor code for edit and save buttons and add some comments.

--- projects/project4/network/static/network/network.js
+++ projects/project4/network/static/network/network.js
@@ -1,40 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    // document.querySelector('#edit_button').addEventListener();
     document.querySelectorAll('.js-edit_button').forEach((button)=> {
-        button.addEventListener('click', (event)=>{
-            event.preventDefault();
+        button.addEventListener('click', ()=>{
             const post = button.parentElement;
-            const post_content = post.querySelector('.js-post-content');
             
-            const textarea = document.createElement('textarea');
-            textarea.style.display = 'block';
-            textarea.style.marginBottom = '10px';
-            textarea.textContent = post_content.innerHTML;
-            textarea.autofocus = true;
-            textarea.rows = "5";
-            textarea.cols = "60";
-
+            // Replaces the post content with a textarea that prefills with the old content
+            const post_content = post.querySelector('.js-post-content');
+            const textarea = create_text_area(post_content.innerHTML);
             post_content.replaceWith(textarea);
+
+            // Replace the edit button with a newly created save button
             button.style.display = 'none';
-
-            const save_button = document.createElement('button');
-            save_button.textContent = 'save';
-            save_button.className = 'btn btn-sm btn-danger';
-
+            const save_button = create_save_button();
             timestamp_div = post.querySelector('.post-timestamp');
             post.insertBefore(save_button, timestamp_div);
 
+            // Clicking the save button updates the post content in both frontend and backend
             const post_id = button.dataset.postId;
-            
-            save_button.addEventListener('click', (e)=>{
-                // e.preventDefault();
+            save_button.addEventListener('click', ()=>{
+                // Update the post content in the frontend
                 textarea_new = post.querySelector('textarea');
                 post_content.innerHTML = textarea_new.value;
                 textarea_new.replaceWith(post_content);
-                button.style.display = 'block';
-                save_button.remove();
-
+                // Update the post content in the backend
                 fetch('/edit/' + post_id, {
                     method: 'PUT',
                     body: JSON.stringify({
@@ -47,8 +35,28 @@ document.addEventListener('DOMContentLoaded', function() {
                 .catch(error => {
                     console.log('Error:', error);
                 });
-
+                // Replace the save button with the edit button.
+                button.style.display = 'block';
+                save_button.remove();
             });
         });
     });
 });
+
+function create_text_area(content) {
+    const textarea = document.createElement('textarea');
+    textarea.style.display = 'block';
+    textarea.style.marginBottom = '10px';
+    textarea.textContent = content;
+    textarea.autofocus = true;
+    textarea.rows = "5";
+    textarea.cols = "60";
+    return textarea;
+}
+
+function create_save_button() {
+    const save_button = document.createElement('button');
+    save_button.textContent = 'save';
+    save_button.className = 'btn btn-sm btn-danger';
+    return save_button;
+}
