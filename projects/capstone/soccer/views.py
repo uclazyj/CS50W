8e61b696b1da959d1286d6fb553cb77b978f625f
yujianzhao <zhaoyujian@ucla.edu>
2025-03-02 15:43:47 -0500

    Add login, logout and register views.

--- projects/capstone/soccer/views.py
+++ projects/capstone/soccer/views.py
@@ -1,5 +1,66 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+
+
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request, "soccer/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        print(username)
+        password = request.POST["password"]
+        print(password)
+        user = authenticate(request, username=username, password=password)
+
+        if user == None:
+            print("User is None")
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "soccer/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "soccer/login.html")
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("index")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "soccer/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "soccer/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "soccer/register.html")
