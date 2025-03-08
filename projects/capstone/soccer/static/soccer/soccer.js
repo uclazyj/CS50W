32223ddb6cbc1092a298f7cd187c3f8f963a02dc
yujianzhao <zhaoyujian@ucla.edu>
2025-03-08 16:08:13 -0500

    Constrain the movement of draggable elements within the screen.

--- projects/capstone/soccer/static/soccer/soccer.js
+++ projects/capstone/soccer/static/soccer/soccer.js
@@ -13,14 +13,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
         draggable.addEventListener('mousedown', function(e) {
             const mouse_initial_x = e.clientX;
             const mouse_initial_y = e.clientY;
-            const draggable_initial_x = draggable.getBoundingClientRect().left;
-            const draggable_initial_y = draggable.getBoundingClientRect().top;
+            const draggable_initial = draggable.getBoundingClientRect();
 
             function onMouseMove(event) {
                 const mouse_current_x = event.pageX;
                 const mouse_current_y = event.pageY;
-                draggable.style.left = draggable_initial_x + mouse_current_x - mouse_initial_x + 'px';
-                draggable.style.top = draggable_initial_y + mouse_current_y - mouse_initial_y + 'px';
+                draggable_final_left = draggable_initial.left + mouse_current_x - mouse_initial_x;
+                draggable_final_top = draggable_initial.top + mouse_current_y - mouse_initial_y;
+
+                draggable_final_left = Math.max(draggable_final_left, 0);
+                draggable_final_left = Math.min(draggable_final_left, window.innerWidth - draggable.offsetWidth);
+
+                draggable_final_top = Math.max(draggable_final_top, 0);
+                draggable_final_top = Math.min(draggable_final_top, window.innerHeight - draggable.offsetHeight);
+
+                draggable.style.left = draggable_final_left + 'px';
+                draggable.style.top = draggable_final_top + 'px';
             }
 
             document.addEventListener('mousemove', onMouseMove);
