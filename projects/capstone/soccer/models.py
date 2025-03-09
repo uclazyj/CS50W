c3b84410eee13752b2e35e26249f03270ffa598d
yujianzhao <zhaoyujian@ucla.edu>
2025-03-09 13:00:26 -0400

    Add unique constraint to PlayerIcon name.

--- projects/capstone/soccer/models.py
+++ projects/capstone/soccer/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     pass
 
 class PlayerIcon(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     x = models.IntegerField(null=True)
     y = models.IntegerField(null=True)
     team_id = models.IntegerField(null=True)
