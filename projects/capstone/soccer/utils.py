803990460ab5f48803f4b752b50c34366d6d746a
yujianzhao <zhaoyujian@ucla.edu>
2025-04-03 00:19:22 -0400

    Add a utility function to handle withdraws and new signup based on the players with season pass.
    The players with season pass are hard coded in the function. It is intended to work only for 2025 spring season for Secaucus soccer team.

--- projects/capstone/soccer/utils.py
+++ projects/capstone/soccer/utils.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import pytesseract
+import re
 
 all_names = set(['柴堰尤', '赵家伟', 'zwb', 'lyx', '赵泽宇', 'szk', 'Congee', 'ruilin', '谢鑫喆', 'Ziyan', 'Alex', '青木堂', '严智勇', 'Ye xie', 'David', '师宇豪', '费昊然', '嘉兴', '余永豪', 'Argentina', 'Steven', 'dyk', '陶', 'Louis', 'Fabien', '陈诗玮', '周裕人', '王宇煊', 'Alan', '郭希', 'lkl', '飓风先生', '钟潏晨', '管住嘴迈开腿', '励天一', 'yucheng', '黄泽宇', '杨冠群', '赵宇健', '朱总', '李翼展', 'Tiger', '谢天石', '月下柠檬树', '伍琨', 'Frank'])
 
@@ -54,3 +55,24 @@ def extract_names_from_image(image_path):
             words2.append(first_two_chars_to_name[word[:2]])
 
     return words2
+
+def get_attendance_list(raw_text):
+    season_pass_names = ["曹彬","xiaosong","伍琨","风","昊天","赵宇健","王宇煊","郭希","怀博群","魔术猪","小苏","Du","子恒","Di","李翼展","泽辉","梁育诚","残风","猛哥","Shawn","邹明昊","GRH","瓜瓜","qgx"]
+
+    withdraw_and_signup_names_text = raw_text.split("甩坑")[1].strip()
+    withdraw_names_text, signup_names_text = withdraw_and_signup_names_text.split("抢坑")
+    withdraw_names = [name.strip() for name in re.split(r'[:：.123456789]', withdraw_names_text)]
+    withdraw_names = set([name for name in withdraw_names if name != ""])
+    
+    attendance_names = [name for name in season_pass_names if name not in withdraw_names]
+    
+    signup_names = signup_names_text.split("paid")
+    excluded_chars = set([' ', '(', ')', '（', '）', ':', '：','.','1','2','3','4','5','6','7','8','9','0'])
+    for raw_name in signup_names:
+        chars = [c for c in raw_name if c not in excluded_chars]
+        name = "".join(chars)
+        if name != "":
+            attendance_names.append(name)
+
+    return attendance_names
+
